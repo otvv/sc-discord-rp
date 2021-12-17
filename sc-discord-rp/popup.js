fad7a127b35fe61c3089fa28b52f76f0a70db468
Otavio <suizld@pm.me>
2021-12-17 15:37:30 -0300

    UI Update
    - Bumped package version.
    - Updated UI to better fit ChromeOS design.
    - Improved UI logic.

--- sc-discord-rp/popup.js
+++ sc-discord-rp/popup.js
@@ -3,7 +3,91 @@
 
 'use strict'
 
-// reload extension when the user clicks on the extension logo
+// close popup window
+document.getElementById('closeButton').onclick = () => {
+	window.close()
+}
+
+// reload extension
 document.getElementById('reloadButton').onclick = () => {
 	chrome.runtime.reload()
+}
+
+// show extension info
+document.getElementById('infoButton').onclick = () => {
+
+	// main card
+	const mainCard = document.getElementById('mainCard')
+
+	// caret switch
+	const customCaret = document.getElementById('caretSwitch')
+
+	// info button
+	const infoButton = document.getElementById('infoButton')
+
+	// info button icon
+	const infoButtonIcon = document.getElementById('infoButtonIcon')
+
+	// paragraph to hide
+	const customText = document.getElementById('hiddenText')
+
+	if (customText.style.display !== 'none') {
+		customText.style.display = 'none'
+	}
+	else {
+		customText.style.display = 'inline-block'
+	}
+
+	if (customText.style.display === 'none') {
+		mainCard.style.height = '100px'
+
+		customCaret.textContent = 'expand_less' // up arrow
+	}
+	else {
+		mainCard.style.height = '180px'
+
+		customCaret.textContent = 'expand_more' // down arrow
+	}
+
+	if (infoButton.checked) {
+		infoButton.style.background = 'rgb(138,180,248)'
+
+		infoButtonIcon.style.color = 'rgb(34,35,40)'
+	}
+	else {
+		infoButton.style.background = 'rgba(160, 160, 160, 0.5)'
+
+		infoButtonIcon.style.color = 'rgb(224, 224, 224)'
+	}
+}
+
+// show extension info when clicking on the caret
+document.getElementById('caretSwitch').onclick = () => {
+
+	// main card
+	const mainCard = document.getElementById('mainCard')
+
+	// caret switch
+	const customCaret = document.getElementById('caretSwitch')
+
+	// paragraph to hide
+	const customText = document.getElementById('hiddenText')
+
+	if (customText.style.display !== 'none') {
+		customText.style.display = 'none'
+	}
+	else {
+		customText.style.display = 'inline-block'
+	}
+
+	if (customText.style.display === 'none') {
+		mainCard.style.height = '100px'
+
+		customCaret.textContent = 'expand_less' // up arrow
+	}
+	else {
+		mainCard.style.height = '180px'
+
+		customCaret.textContent = 'expand_more' // down arrow
+	}
 }
